0541fb6e871bd170e1bf7d577832c78efc469b3f
Kieran Kuhn <kuhn.kieran@gmail.com>
2017-08-24 23:27:50 -0400

    firstCommit

---
+++ ClozeCard.js
@@ -0,0 +1,20 @@
+var ClozeCard = function(fullText, partial){
+	this.fullText = fullText;
+	this.partial = partial;
+	this.partialArray = partial.split(" ");
+	this.fullTextArray = fullText.split(" ");
+	for(var i=0;i<this.fullTextArray.length;i++){
+		if(this.fullTextArray[i] == this.partialArray[0]){
+			this.fullTextArray[i] = ".....";
+		}
+	};
+	for(var i=0;i<this.fullTextArray.length;i++){
+		if(this.fullTextArray[i] == this.partialArray[1]){
+			this.fullTextArray[i] = ".....";
+		}
+	};
+	this.cloze = this.fullTextArray.join(" ");
+}
+
+module.exports = ClozeCard;
+
